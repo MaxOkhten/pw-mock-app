9c17abe61c84864da15a3d46493d0be04d66085c
MaxOkhten <maxokhten@gmail.com>
2026-01-08 18:04:15 +0100

    update tests

---
+++ tests/test.spec.ts
@@ -0,0 +1,18 @@
+import {test} from "@playwright/test"
+
+test.beforeAll(() => {
+    
+})
+
+test.beforeEach(async({page}) => {
+    await page.goto("http://localhost:4200/")
+    await page.getByText("Forms").click()
+})
+
+test("test #1", async ({page}) => {
+    await page.getByText("Form Layouts").click()
+})
+
+test("navigate to datepicker page", async ({page}) => {
+    await page.getByText("Datepicker").click()
+})
