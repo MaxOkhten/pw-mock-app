0d3e361286a3d9760b52afc41d79de327006130f
MaxOkhten <maxokhten@gmail.com>
2026-01-09 19:01:13 +0100

    locators

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,18 +1,34 @@
 import {test} from "@playwright/test"
 
-test.beforeAll(() => {
-    
-})
-
 test.beforeEach(async({page}) => {
     await page.goto("http://localhost:4200/")
     await page.getByText("Forms").click()
-})
-
-test("test #1", async ({page}) => {
     await page.getByText("Form Layouts").click()
 })
 
-test("navigate to datepicker page", async ({page}) => {
-    await page.getByText("Datepicker").click()
+test("Locator syntax rules", async({page}) => {
+    //by Tag name
+    page.locator("input")
+
+    //by ID
+    page.locator("#inputEmail1")
+
+    //by Class value
+    page.locator("shape-rectangle")
+
+    //by attribute
+    page.locator('[placeholder="Email"]')
+
+    //combine selectors
+    page.locator('input[placeholder="Email"]')
+
+    //XPath
+    page.locator('//*[@id="inputEmail1')
+
+    //by text match
+    page.locator('text-is("Using the Grid")')
+
+    //by partial text match
+    page.locator(':text("Using")')
+
 })
