0b1826a5944acc66896ff7d61dcb2cd70bf1444e
MaxOkhten <maxokhten@gmail.com>
2026-01-19 17:37:36 +0100

    radio buttons

--- tests/components.spec.ts
+++ tests/components.spec.ts
@@ -26,4 +26,28 @@ test.describe('Form Layouts Test Suite', () => {
         //locator assertions
         await expect(emailInput).toHaveValue('hello2@gmail.com')
     })
+
+    test('radio buttons', async({page}) => {
+        const usingTheGrid = page.locator('nb-card', {hasText: "Using the Grid"})
+
+        //by Label
+        await usingTheGrid.getByLabel('Option 1').check({force: true}) // {force: true} - if the label is hidden
+
+        //by Role
+        await usingTheGrid.getByRole('radio', {name: "Option 1"}).check({force: true})
+
+        //validation - by role
+        const radioStatus = await usingTheGrid.getByRole('radio', {name: "Option 1"}).isChecked()
+        expect(radioStatus).toBeTruthy()
+
+        //validation - locator assertion
+        await expect(await usingTheGrid.getByRole('radio', {name: "Option 1"})).toBeChecked()
+
+        //check that the option 1 is unchecked
+        await usingTheGrid.getByRole('radio', {name: "Option 2"}).check({force: true})
+        expect(await usingTheGrid.getByRole('radio', {name: "Option 1"}).isChecked()).toBeFalsy()
+        expect(await usingTheGrid.getByRole('radio', {name: "Option 2"}).isChecked()).toBeTruthy()
+    })
 })
+
+    
