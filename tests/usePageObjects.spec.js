5e6d0e3787cb38fff4fc10ff5b06102435d76cfd
MaxOkhten <maxokhten@gmail.com>
2026-02-20 17:12:57 +0100

    add pageManager

--- tests/usePageObjects.spec.js
+++ tests/usePageObjects.spec.js
@@ -1,32 +1,31 @@
 import {test, expect} from "@playwright/test"
-import {NavigationPage} from "../page-objects/navigationPage"
-import {FormLayoutsPage} from "../page-objects/formLayoutsPage"
-import {DatepickerPage} from "../page-objects/datePicker"
+import {PageManager} from "../page-objects/pageManager"
+// import {NavigationPage} from "../page-objects/navigationPage"
+// import {FormLayoutsPage} from "../page-objects/formLayoutsPage"
+// import {DatepickerPage} from "../page-objects/datePicker"
 
 test.beforeEach(async({page}) => {
     await page.goto("http://localhost:4200/")
 })
 
 test("Go to form page", async({page}) => {
-    const navigateTo = new NavigationPage(page)
-    await navigateTo.formLayoutsPage()
-
-    await navigateTo.datepickerPage()
-    await navigateTo.smartTablePage()
-    await navigateTo.toastrPage()
-    await navigateTo.tooltipPage()
+    const pm = new PageManager(page)
+    
+    await pm.navigateTo().formLayoutsPage()
+    await pm.navigateTo().datepickerPage()
+    await pm.navigateTo().smartTablePage()
+    await pm.navigateTo().toastrPage()
+    await pm.navigateTo().tooltipPage()
 })
 
 test("Paramethrized methods", async({page}) => {
-    const navigateTo = new NavigationPage(page)
-    const onFormLayoutsPage = new FormLayoutsPage(page)
-    const onDatePickerPage = new DatepickerPage(page)
- 
-    await navigateTo.formLayoutsPage()
-    await onFormLayoutsPage.submitUsingTheGridForm('hello@hello.com', 'P@s$w0rD', 'Option 1')
-    await onFormLayoutsPage.submitInlineForm('Mister X', "mister@mister.com", true)
-    
-    await navigateTo.datepickerPage()
-    await onDatePickerPage.selectCommonDatepickerFromToday(5)
-    await onDatePickerPage.selectDatepickerWithRangeFromToday(6, 15)
+    const pm = new PageManager(page)
+
+    await pm.navigateTo().formLayoutsPage()
+    await pm.onFormLayoutsPage().submitUsingTheGridForm('hello@hello.com', 'P@s$w0rD', 'Option 1')
+    await pm.onFormLayoutsPage().submitInlineForm('Mister X', "mister@mister.com", true)
+    await pm.navigateTo().datepickerPage()
+    await pm.onDatePickerPage().selectCommonDatepickerFromToday(5)
+    await pm.onDatePickerPage().selectDatepickerWithRangeFromToday(6, 15)
 })
+
