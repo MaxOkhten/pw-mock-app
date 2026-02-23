49e4d97f1094c5532fa6e53f9f265f0c9764a07e
MaxOkhten <maxokhten@gmail.com>
2026-02-23 16:54:12 +0100

    fix HelperBase

--- page-objects/helperBase.ts
+++ page-objects/helperBase.ts
@@ -1,7 +1,7 @@
 import { Page, expect } from "@playwright/test";
 
 export class HelperBase {
-    private readonly page: Page
+    readonly page: Page
 
     constructor(page: Page) {
         this.page = page
