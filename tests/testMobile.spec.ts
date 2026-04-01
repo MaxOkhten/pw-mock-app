d5845ab49c782343e3c8263518eb909c5616a30e
MaxOkhten <maxokhten@gmail.com>
2026-04-01 18:37:17 +0200

    add mobile emulator

---
+++ tests/testMobile.spec.ts
@@ -0,0 +1,23 @@
+import {test, expect} from "@playwright/test";
+
+test("input fields", async({page}) => {
+
+        await page.goto("/");
+        await page.getByText("Forms").click();
+        await page.getByText("Form Layouts").click();
+
+        const emailInput = page.locator('nb-card', {hasText: "Using the Grid"}).getByRole('textbox', {name: "Email"})
+
+        await emailInput.fill('hello@gmail.com')
+        await emailInput.clear()
+
+        //simulate keystrokes
+        await emailInput.pressSequentially('hello2@gmail.com', {delay: 200})
+
+        //generic assetions - checks any value without auto-waiting
+        const inputValue = await emailInput.inputValue()
+        expect(inputValue).toEqual('hello2@gmail.com') 
+
+        //locator assertions
+        await expect(emailInput).toHaveValue('hello2@gmail.com')
+    });
