8e3d9e7d1c209eee63d92a758c959f75a2e76217
MaxOkhten <maxokhten@gmail.com>
2026-04-02 23:52:46 +0200

    fix mobile

--- tests/testMobile.spec.ts
+++ tests/testMobile.spec.ts
@@ -1,6 +1,6 @@
 import {test, expect} from "@playwright/test";
 
-test("input fields", async({page}, testInfo) => {
+test("input fields mobile", async({page}, testInfo) => {
 
         await page.goto("/");
 
